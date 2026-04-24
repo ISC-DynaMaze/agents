7ded69d253fce27f9f4beac3ee349e3f7182fa01
LordBaryhobal <lordbaryhobal@gmail.com>
2026-04-25 00:49:49 +0200

    feat(logger): save bookmarks in IndexedDB

--- src/agents/logger/public/static/bookmarks.mjs
+++ src/agents/logger/public/static/bookmarks.mjs
@@ -6,7 +6,10 @@ export class Bookmark {
      * @param {String} name
      * @param {Message} message
      */
-    constructor(name, message) {
+    constructor(name, message, id = undefined) {
+        if (id !== undefined) {
+            this.id = id
+        }
         this.name = name
         this.message = message
     }
@@ -25,6 +28,83 @@ export class Bookmarks {
 
         this.template = document.getElementById("bookmark-template")
         this.list = this.node.querySelector("#saved-messages")
+
+        this.dbName = "DynaMaze-Logger"
+        this.storeName = "bookmarks"
+        this.dbVersion = 1
+        this.loadBookmarks()
+    }
+
+    /**
+     * @returns {Promise<IDBDatabase>}
+     */
+    openDB() {
+        return new Promise((resolve, reject) => {
+            const dbRequest = indexedDB.open(this.dbName, this.dbVersion)
+            dbRequest.onupgradeneeded = event => {
+                /** @type {IDBDatabase} */
+                const db = event.target.result
+
+                if (!db.objectStoreNames.contains(this.storeName)) {
+                    db.createObjectStore(this.storeName, {
+                        keyPath: "id",
+                        autoIncrement: true
+                    })
+                }
+            }
+
+            dbRequest.onsuccess = event => {
+                resolve(event.target.result)
+            }
+
+            dbRequest.onerror = e => {
+                console.error("Error opening IndexedDB")
+                reject(e.target.error)
+            }
+        })
+    }
+
+    async loadBookmarks() {
+        const db = await this.openDB()
+        const store = db
+            .transaction(this.storeName, "readonly")
+            .objectStore(this.storeName)
+
+        const req = store.getAll()
+        req.onsuccess = event => {
+            const data = event.target.result
+            data.forEach(bm => {
+                const node = this.makeNode(bm)
+                this.list.appendChild(node)
+            })
+        }
+    }
+
+    /**
+     * @param {Bookmark} bookmark
+     */
+    async saveBookmarkToDB(bookmark) {
+        const db = await this.openDB()
+        const store = db
+            .transaction(this.storeName, "readwrite")
+            .objectStore(this.storeName)
+
+        const req = store.put(bookmark)
+        req.onsuccess = () => {
+            bookmark.id = req.result
+        }
+    }
+
+    /**
+     * @param {Bookmark} bookmark
+     */
+    async removeBookmarkFromDB(bookmark) {
+        const db = await this.openDB()
+        const store = db
+            .transaction(this.storeName, "readwrite")
+            .objectStore(this.storeName)
+        
+        const req = store.delete(bookmark.id)
     }
 
     /**
@@ -36,6 +116,7 @@ export class Bookmarks {
         this.bookmarkID++
         const node = this.makeNode(bookmark)
         this.list.appendChild(node)
+        this.saveBookmarkToDB(bookmark)
     }
 
     /**
@@ -47,13 +128,21 @@ export class Bookmarks {
         const node = this.template.cloneNode(true)
         node.removeAttribute("id")
         node.classList.remove("template")
-        node.querySelector(".name").value = bookmark.name
+        const name = node.querySelector(".name")
+        name.value = bookmark.name
+        name.addEventListener("change", () => {
+            bookmark.name = name.value
+            this.saveBookmarkToDB(bookmark)
+        })
         node.querySelector(".recipient").innerText = bookmark.message.recipient
 
         node.querySelector(".send").addEventListener("click", () => {
             this.agent.send(bookmark.message.body, bookmark.message.recipient)
         })
-        node.querySelector(".delete").addEventListener("click", () => node.remove())
+        node.querySelector(".delete").addEventListener("click", () => {
+            node.remove()
+            this.removeBookmarkFromDB(bookmark)
+        })
         return node
     }
 }
