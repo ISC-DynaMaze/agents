29e540997f25412dec71c8caddef4910ae71cb7f
Alexandre <alexandre.venturi@outlook.com>
2026-05-01 14:06:29 +0200

    feat : add save data in file

--- src/agents/controller/obstacles_position.py
+++ src/agents/controller/obstacles_position.py
@@ -5,6 +5,7 @@ from pathlib import Path
 from typing import TYPE_CHECKING
 import time
 import cv2
+import json
 import numpy as np
 from spade.behaviour import OneShotBehaviour
 from common.models.camera import CameraRequest, CameraResponse
@@ -15,7 +16,7 @@ from common.models.common import ReqResAdapter
 
 from agents.controller.maze.detect_obstacles import (
     find_obstacles,
-    draw_detected_obstacles
+    draw_detected_obstacles,
 )
 
 from agents.controller.maze.wall_detection import get_pink_mask, find_outer_rectangle
@@ -26,7 +27,8 @@ if TYPE_CHECKING:
     from agents.controller.agent import ControllerAgent
 
 ROBOT_ARM_POSITION = (394, 328)
-MAZE_SIZE = (2200,695) #Measured by hand (Width, Height)
+MAZE_SIZE = (2200, 695)  # Measured by hand (Width, Height)
+
 
 class ObstacleRelativePositionBehaviour(OneShotBehaviour):
     agent: ControllerAgent
@@ -44,30 +46,43 @@ class ObstacleRelativePositionBehaviour(OneShotBehaviour):
         await self.req_image()
         img = await self.wait_for_new_image(timeout=10.0)
 
-        self.logger.info(f"[Measure] Start calculating pixel distance and real distance on maze border")
+        self.logger.info(
+            f"[Measure] Start calculating pixel distance and real distance on maze border"
+        )
         measure = self.compute_distance(img)
         self.logger.info(f"[Measure] L :{measure[0]}, l : {measure[1]}")
 
-        self.logger.info(f"[Measure] Start calculating pixel distance and real distance on maze border")
+        self.logger.info(
+            f"[Measure] Start calculating pixel distance and real distance on maze border"
+        )
         result = find_obstacles(img, self.agent.maze)
-        blocks = result["block_by_color"]
+        blocks = result["blocks_by_color"]
+        blocks_pos = dict()
         for color, list_of_blocks in blocks.items():
             for block in list_of_blocks:
                 center = block["center"]  # C'est un tuple (x, y)
-                distance_robot = (abs(center[0]-ROBOT_ARM_POSITION[0])*measure[0],abs(center[1]-ROBOT_ARM_POSITION[1])*measure[1])
-                self.logger.info(f"Obstacle {color} founded at {center}, {distance_robot[0]} horizontally away and {distance_robot[1]} vertically away ")
+                distance_robot = (
+                    abs(center[0] - ROBOT_ARM_POSITION[0]) * measure[0],
+                    abs(center[1] - ROBOT_ARM_POSITION[1]) * measure[1],
+                )
+                self.logger.info(
+                    f"Obstacle {color} founded at {center}, {distance_robot[0]} horizontally away and {distance_robot[1]} vertically away "
+                )
+                blocks_pos[color]["x"] = distance_robot[0]
+                blocks_pos[color]["y"] = distance_robot[1]
+        self.save_obstacle_position(blocks_pos)
 
-    def compute_distance(self, img: np.ndarray) -> tuple[float, float] :
+    def compute_distance(self, img: np.ndarray) -> tuple[float, float]:
         self.logger.info(f"[Compute distance] Enter function")
         mask = get_pink_mask(img)
         self.logger.info(f"[Mask] Mask generated")
         sizes = find_outer_rectangle(mask)
         self.logger.info(f"[Measure] Length of sided returned")
-        width = sizes[2] #The longest side
-        height = sizes[3] #The shortest side
+        width = sizes[2]  # The longest side
+        height = sizes[3]  # The shortest side
 
-        ratioW = MAZE_SIZE[0]/width
-        ratioH = MAZE_SIZE[1]/height
+        ratioW = MAZE_SIZE[0] / width
+        ratioH = MAZE_SIZE[1] / height
         return (ratioW, ratioH)
 
     async def req_image(self):
@@ -91,3 +106,8 @@ class ObstacleRelativePositionBehaviour(OneShotBehaviour):
                     f"Error occurred while waiting for camera response: {e}"
                 )
                 continue
+
+    def save_obstacle_position(self, block_pos: dict):
+        filename = self.rel_pos / "obs_pos.json"
+        with open(filename, "w") as f:
+            json.dump(block_pos, f, indent=4)
