bd1547c080565a38d08c1e46ce05d6a33af2e4ba
Alexandre <alexandre.venturi@outlook.com>
2026-05-04 08:12:11 +0200

    feat : init behaviour for robot arm

---
+++ src/agents/robot/remove_obstacle.py
@@ -0,0 +1,16 @@
+from spade.behaviour import OneShotBehaviour
+import json
+from agents.robot.agent import RobotAgent
+
+class RemoveObstacleBehaviour(OneShotBehaviour):
+    agent : RobotAgent
+
+    def __init__(self, ):
+        super().__init__()
+    
+    def on_start(self):
+    
+    def run(self):
+    
+    def read_file(self):
+        with 
