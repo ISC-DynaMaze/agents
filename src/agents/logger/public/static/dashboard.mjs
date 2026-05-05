6f5ca23c8825d0974810b43fe4685651f1011075
LordBaryhobal <lordbaryhobal@gmail.com>
2026-05-05 16:26:29 +0200

    feat(logger): add basic time-keeper logic

--- src/agents/logger/public/static/dashboard.mjs
+++ src/agents/logger/public/static/dashboard.mjs
@@ -4,10 +4,12 @@ import { RobotCamera } from "./bot_camera.mjs";
 import { Logger } from "./logger.mjs";
 import { Maze } from "./maze.mjs";
 import { Sender } from "./sender.mjs";
+import { TimeKeeper } from "./time_keeper.mjs";
 
 export class Dashboard {
     constructor() {
         this.agent = new Agent()
+        this.timeKeeper = new TimeKeeper(this.agent, document.getElementById("time-keeper"))
         this.sender = new Sender(this.agent, document.getElementById("sender"))
         this.bookmarks = new Bookmarks(this.agent, document.getElementById("saved"))
         this.robotCamera = new RobotCamera(this.agent, document.getElementById("bot-cam"))
