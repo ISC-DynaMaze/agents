10e3e7b491128b3cef7bb9a7d5501e945be6d59d
Alexandre <alexandre.venturi@outlook.com>
2026-05-07 13:42:58 +0200

    fix : add image request

--- src/agents/controller/find_avoiding_cell.py
+++ src/agents/controller/find_avoiding_cell.py
@@ -3,7 +3,8 @@ from __future__ import annotations
 import datetime
 import logging
 from pathlib import Path
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Optional
+import numpy as np
 
 import cv2
 from spade.behaviour import OneShotBehaviour
@@ -11,24 +12,39 @@ from spade.behaviour import OneShotBehaviour
 from agents.controller.maze.find_path import a_star_search
 from agents.controller.maze.grid import Maze
 from agents.controller.send_direction import SendDirectionBehaviour
+from common.models.camera import CameraRequest, CameraResponse
+from common.sender import BaseSenderBehaviour
+from common.utils import wait_for_response
 
 if TYPE_CHECKING:
     from agents.controller.agent import ControllerAgent
 
 
 class FindAvoidingCellBehaviour(OneShotBehaviour):
+
     agent: ControllerAgent
+
     def __init__(self, maze: Maze):
         super().__init__()
         self.logger = logging.getLogger("FindAvoidingCell")
         self.maze = maze
     
     async def run(self) -> None:
-        opp_start = (self.agent.opponent_current_cell.row, self.agent.opponent_current_cell.col)
-        opp_target = (self.maze.opponent_target_cell.row, self.maze.opponent_target_cell.col)
 
-        
-        opponent_path = a_star_search(self.maze, opp_start, opp_target)
+        await self.req_image()
+        img: Optional[np.ndarray] = await self.wait_for_new_image(timeout=10.0)
+        if img is None:
+            self.logger.error("Timed out waiting for camera image")
+            return
+
+        self.opponent_current_cell = self.get_opponent_current_cell(img)
+
+        opp_start = (self.opponent_current_cell.row, self.opponent_current_cell.col)
+        opp_target = (self.maze.opponent_target_cell.row, self.maze.opponent_target_cell.col)
+        self.logger.info(f"[Opponent] Robot position : {opp_start}, Robot target : {opp_target}")
+
+        self.agent.opponent_path= a_star_search(self.maze, opp_start, opp_target)
+        self.logger.info(f"[Opponent] Path : {self.agent.opponent_path}")
 
         current = self.maze.bot_cell
         safe_cell = None
@@ -43,11 +59,12 @@ class FindAvoidingCellBehaviour(OneShotBehaviour):
                     break 
 
         if safe_cell:
-            self.logger.info(f"New temporary destination of robot avoidance : {safe_cell}")
+            self.logger.info(f"New temporary destination of bot avoidance : {safe_cell}")
             new_path = a_star_search(self.maze, (current.row, current.col), (safe_cell.row, safe_cell.col))
             if new_path :
                 self.agent.current_path = new_path
                 self.agent.add_behaviour(SendDirectionBehaviour())
+                self.logger.info(f"[New Path] {self.agent.current_path}")
 
 
     def get_opponent_current_cell(self, image):
@@ -62,11 +79,25 @@ class FindAvoidingCellBehaviour(OneShotBehaviour):
             
             for i, marker_id in enumerate(ids.flatten()):
                 if marker_id not in known_ids:
-                    # Récupérer le centre du marqueur (pixels)
                     c = corners[i][0]
                     center_x = int(c[:, 0].mean())
                     center_y = int(c[:, 1].mean())
                     row, col = self.maze.pixel_to_cell(center_x, center_y)
                     return self.maze.get_cell(row, col)
         return None
+
+    async def req_image(self):
+        req = CameraRequest()
+        self.agent.add_behaviour(BaseSenderBehaviour(req, str(self.agent.camera_jid)))
+
+    async def wait_for_new_image(self, timeout: float) -> Optional[np.ndarray]:
+        res: Optional[CameraResponse] = await wait_for_response(
+            self, CameraResponse, timeout
+        )
+        if res is None:
+            self.logger.error("Timed out waiting for camera response message")
+            return None
+        save_dir = Path("photos")
+        img, _ = await res.decode_img(save_dir)
+        return img
     
