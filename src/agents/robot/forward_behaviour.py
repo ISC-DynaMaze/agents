63b0398a9dd6ee8d23f6524fa64a6ddbb6370b56
LordBaryhobal <lordbaryhobal@gmail.com>
2026-05-08 08:24:36 +0200

    fix(robot): disable front IR sensors in forward behaviour

--- src/agents/robot/forward_behaviour.py
+++ src/agents/robot/forward_behaviour.py
@@ -23,29 +23,7 @@ class ForwardBehaviour(CyclicBehaviour):
         return self.agent.bot
 
     async def run(self) -> None:
-        left_free: bool = self.bot.getIRSensorLeft()
-        right_free: bool = self.bot.getIRSensorRight()
-
-        speed_left: int = self.speed
-        speed_right: int = self.speed
-
-        sides: tuple[bool, bool] = (left_free, right_free)
-
-        match sides:
-            case (False, False):
-                speed_left = 0
-                speed_right = 0
-
-            case (False, True):
-                speed_right = 0
-
-            case (True, False):
-                speed_left = 0
-
-        if speed_left == 0 and speed_right == 0:
-            self.bot.stop()
-        else:
-            self.bot.setPWMA(speed_left * self.agent.wheel_adjustements.left_factor)
-            self.bot.setPWMB(speed_right * self.agent.wheel_adjustements.right_factor)
+        self.bot.setPWMA(self.speed * self.agent.wheel_adjustements.left_factor)
+        self.bot.setPWMB(self.speed * self.agent.wheel_adjustements.right_factor)
 
         await asyncio.sleep(0.05)
