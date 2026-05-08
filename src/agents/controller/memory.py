7a8fbce7ba76aec1535a7a3c3a7df86243da917a
LordBaryhobal <lordbaryhobal@gmail.com>
2026-05-08 11:24:46 +0200

    fix(controller): swap maze coordinates in controller memory

--- src/agents/controller/memory.py
+++ src/agents/controller/memory.py
@@ -19,10 +19,10 @@ class Memory:
         i1: int = self.ORIENTATION_ORDER.index(self.last_orientation)
         i2: int = (i1 + rotation) % 4
         self.last_orientation = self.ORIENTATION_ORDER[i2]
-        x, y = self.last_pos
+        y, x = self.last_pos
         dx, dy = displacement
         dx, dy = self._rotate_displacement(dx, dy, i1)
-        self.last_pos = (x + dx, y + dy)
+        self.last_pos = (y + dy, x + dx)
 
     def _rotate_displacement(self, dx: int, dy: int, rot: int) -> tuple[int, int]:
         match rot:
